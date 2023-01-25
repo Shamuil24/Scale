992b2bffe9eb690bbd55cfcc757348b5f6c7f6c7
Shamuil24 <dimash.adel@gmail.com>
2023-01-25 21:21:50 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menumobile = document.querySelector('.menu-mobile')
+
+menuBtn.addEventListener("click", () => {
+    menumobile.classList.toggle("open");
+}
+)   
